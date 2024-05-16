a865650a05de6c21fb568ab4847d26ebc48ff6e0
kayyza <rachel.herron@live.ca>
2024-05-16 14:38:40 -0400

    Added Equals, Getter, Setter to my Student class using lombok

--- FinalProject/src/main/java/org/example/Student.java
+++ FinalProject/src/main/java/org/example/Student.java
@@ -1,5 +1,12 @@
 package org.example;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@EqualsAndHashCode
+@Getter
+@Setter
 public class Student {
     private String studentId;
     private String studentName;
@@ -8,4 +15,34 @@ public class Student {
     private Department department;
     private Course[] registeredCourses;
     private static int nextId;
+
+    //TODO:
+    // 1. boolean registerCourse(Course course)
+    // registers a course, this method (1) adds the course
+    // to the student's registeredCourses list,
+    // (2) adds the student to the course's registeredStudents list,
+    // (3) appends a null for the scores of each assignment of the course.
+    // If the course is already registered, directly returns false
+
+    //TODO:
+    // boolean dropCourse(Course course)
+    // drops a course, remove the course from the student's registeredCourses list,
+    // and remove the student from the course's registeredStudents list.
+    // If the course is not registered yet, directly returns false
+
+    //TODO:
+    // Constructor with studentName, gender, address, and department,
+    // it will create a student with studentId automatically generated based on the nextId,
+    // and registeredCourses as empty object
+
+    //TODO:
+    // toSimplifiedString
+    // converts a student to a simple string with only the studentId,
+    // the studentName, and departmentName. This method is called in Course toString().
+
+    //TODO:
+    // toString
+    // converts a student to a string that contains the studentId, the studentName,
+    // the gender, the address and the department, and the registeredCourses
+    // (only the courseId, the courseName, and the departmentName)
 }
