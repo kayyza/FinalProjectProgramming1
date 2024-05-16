854bc42cca6fdb487fd996f19784be97538c6c05
kayyza <rachel.herron@live.ca>
2024-05-16 14:32:16 -0400

    Added ToString, Equals, Getter, Setter to my Department class using lombok

--- FinalProject/src/main/java/org/example/Department.java
+++ FinalProject/src/main/java/org/example/Department.java
@@ -1,7 +1,24 @@
 package org.example;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@EqualsAndHashCode
+@Getter
+@Setter
 public class Department {
     private String departmentId;
     private String departmentName;
     private static int nextId;
+
+    //TODO:
+    // 1. static boolean validateDepartmentName(String departmentName)
+    // checks if a department name is valid or not,
+    // a department name should only contain letters or space
+    // 2. Constructor
+    // if the departmentName is invalid, create the object with everything as null
+
 }
