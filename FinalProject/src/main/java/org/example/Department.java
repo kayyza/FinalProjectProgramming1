8c31f3ec07bdaf8314a47495b7305544ecccfd63
kayyza <rachel.herron@live.ca>
2024-05-17 11:02:59 -0400

    Completed validateDepartmentName method in the Department class

--- FinalProject/src/main/java/org/example/Department.java
+++ FinalProject/src/main/java/org/example/Department.java
@@ -14,11 +14,25 @@ public class Department {
     private String departmentName;
     private static int nextId;
 
+
+    public static boolean validateDepartmentName(String departmentName) {
+        for(int i = 0; i < departmentName.length(); i++){
+            char character = departmentName.charAt(i);
+            if (Character.isLetter(character) || character == ' ') {
+                continue;
+            }
+            return false;
+        }
+        return true;
+    }
+
     //TODO:
-    // 1. static boolean validateDepartmentName(String departmentName)
+    // static boolean validateDepartmentName(String departmentName)
     // checks if a department name is valid or not,
     // a department name should only contain letters or space
-    // 2. Constructor
+
+    //TODO:
+    // Constructor
     // if the departmentName is invalid, create the object with everything as null
 
 }
